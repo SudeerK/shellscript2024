1ae1aeaf673ebd960d3fca4f096da986cef4dc14
SudeerK <sudeercc.23@gmail.com>
2024-05-20 19:09:34 +0530

    practice

--- 07-1-Arrays.sh
+++ 07-1-Arrays.sh
@@ -5,4 +5,4 @@ fruits=("Banana" "Apple" "Guava")
 echo "First Fruit is : ${fruits[0]}"
 echo "Second Fruit is : ${fruits[1]}"
 
-echo "All fruits are : ${fruits[@]}
+echo "All fruits are : ${fruits[@]}"
