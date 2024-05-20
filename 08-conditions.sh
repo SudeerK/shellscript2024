efaf6f98d02c76469a64fbdfc3092ba48123417d
SudeerK <sudeercc.23@gmail.com>
2024-05-20 19:34:01 +0530

    practice

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -2,9 +2,18 @@
 
 Number=$1
 
+<<comment
 if (($Number>100))
 then 
 echo "Given Number:$Number is greater than 100"
 else
 echo "Given Number:$Number is less than 100"
+fi
+comment
+
+if [ $Number>50 ]
+then 
+echo "Given Number:$Number is greater than 50"
+else
+echo "Given Number:$Number is less than 50"
 fi
