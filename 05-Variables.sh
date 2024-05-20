f184b0fe82450abc494528350cf16aa10e5dd2f9
SudeerK <sudeercc.23@gmail.com>
2024-05-20 13:39:46 +0530

    Variables modified

--- 05-Variables.sh
+++ 05-Variables.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# If we write the code like this then we can see the username and password entered while executing in terminal.
+#echo "Enter the username: "
+#read Username
+#echo "Enter Password: "
+#read Password
+#echo " Username is: $Username, Password is : $Password"
+
 echo "Enter the username: "
-read Username
-
+read -s Username
 echo "Enter Password: "
-read Password
-
+read -s Password
 echo " Username is: $Username, Password is : $Password"
