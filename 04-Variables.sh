65b43e862fd3454bb88abd590cbfac19880d6ce2
SudeerK <sudeercc.23@gmail.com>
2024-05-20 13:19:54 +0530

    Variables modified

--- 04-Variables.sh
+++ 04-Variables.sh
@@ -2,8 +2,8 @@
 
 # By passing arguments while executing the script.
 
-$PERSON1=$1
-$PERSON2=$2
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1::Hello $PERSON2! Good Morning"
 echo "$PERSON2::Hi $PERSON1! Very Good Morning"
