49da5ee89978a1b5551f5152be80cd38b0f1d0d2
SudeerK <sudeercc.23@gmail.com>
2024-05-21 19:54:38 +0530

    conditions

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -10,6 +10,7 @@ else
 echo "Given Number:$Number is less than 100"
 fi
 comment
+# we can give comment as above as well. Instead of word comment it can be any oth er word.
 
 if [ $Number -gt 50 ]
 then 
