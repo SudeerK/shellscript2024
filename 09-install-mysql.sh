21c4ab5bd58bfadebfe8e5e2b6a3389ed2b1c824
SudeerK <sudeercc.23@gmail.com>
2024-05-21 08:33:54 +0530

    conditions for installing mySQL

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -3,8 +3,9 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
 echo "Error:: Please run this script with root user."
+exit 1 #Any value other than 0 can be given so that it terminates here
 else
 echo " You are a Root user"
-fi
+fi #reverse of if indicating end.
 
 yum install mysql-y
