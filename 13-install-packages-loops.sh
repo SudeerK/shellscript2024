f50e5a23be1c2e6836fb102946a277951aaf3d93
SudeerK <sudeercc.23@gmail.com>
2024-05-21 20:08:39 +0530

    packages with loops

--- 13-install-packages-loops.sh
+++ 13-install-packages-loops.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 if [ $ID -ne 0 ]
 then 
-echo "not a root user"
+echo -e "$R ERROR: $N Not a root user"
 else
 echo "root user"
 fi
